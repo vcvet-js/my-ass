d4dc12ec2380bc69753c34e8e58a4026b62dc761
vcvet-js <doktor.gevorkyan@gmail.com>
2023-05-31 20:45:29 +0300

     Моя первая домашка

---
+++ Домашки /1.1/homeWork.js
@@ -0,0 +1,13 @@
+const userFirstName = prompt('Введите ваше Имя:','Евгений')
+const userSecondName = prompt('Введите вашу Фамилию:','Пригожин')
+const userAge = prompt('Введите ваш возвраст:','сколько вам лет бля?')
+const userLang = prompt ('какой ваш любимый язык программирования?','')
+
+console.log( `Участник: ${userFirstName} ${userSecondName}
+                Возвраст: ${userAge}
+                Язык программирования: ${userLang}`
+                );
+document.write(`Участник: ${userFirstName} ${userSecondName}
+Возвраст: ${userAge}
+Язык программирования: ${userLang}`)
+
