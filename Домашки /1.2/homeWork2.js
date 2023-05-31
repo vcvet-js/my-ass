38b335e2f3fc766fb1edcccdef9d6e0f8ab66134
vcvet-js <doktor.gevorkyan@gmail.com>
2023-05-31 21:22:22 +0300

    все домашки

---
+++ Домашки /1.2/homeWork2.js
@@ -0,0 +1,4 @@
+let firstNumber = +prompt('Vvedite Pervoe')
+let secondNumber = +prompt('Vvedite Vtoroe')
+let thirdNumber = +prompt('Vvedite trete')
+alert(firstNumber+secondNumber+thirdNumber);
---
+++ Домашки /1.3/homeWork3.js
@@ -0,0 +1,4 @@
+let firstKatet = +prompt('Введите длину первого катета')
+let secondKatet = +prompt('Введите длину второго катета')
+let ploshad = (firstKatet*secondKatet)/2
+alert(`Площадь прямоугольного треугольника равна ${ploshad} квадратных условных единиц`)
